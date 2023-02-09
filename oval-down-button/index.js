6d2125204b67786e0a7b0d4e2948927832aa1498
XianFengWu1996 <46692996+XianFengWu1996@users.noreply.github.com>
2023-02-08 20:28:39 -0500

    set up oval button svg

---
+++ oval-down-button/index.js
@@ -0,0 +1,14 @@
+const btn = document.getElementById('down_nav_btn');
+const outer = btn.querySelector('#outer');
+const wave = btn.querySelector('#wave');
+const arrow = btn.querySelector('#arrow');
+
+const initialRun = false;
+
+// setInterval(() => {
+//     initialRun = true;
+
+//   outer.classList.toggle('animate-outer');
+//   wave.classList.toggle('animate-wave');
+//   arrow.classList.toggle('animate-arrow');
+// }, 4000);
